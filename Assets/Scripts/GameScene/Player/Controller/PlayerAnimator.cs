f6d7f4fd3f5510165e8e73c716c262d885217782
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-02-22 22:19:51 +0900

    SOL-138 <애니메이션 본작업 1차(방향에 따른 애니메이션) >
    순환 Idle 애니메이션 및 각 방향의 따른 애니메이션

--- Assets/Scripts/GameScene/Player/Controller/PlayerAnimator.cs
+++ Assets/Scripts/GameScene/Player/Controller/PlayerAnimator.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
+    private int _horizontalHash;
+    private int _verticalHash;
+    private int _isIdleHash;
+    private int _isJumpingHash;
+    private int _isFallingHash;
     // Start is called before the first frame update
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _horizontalHash = Animator.StringToHash("Horizontal");
+        _verticalHash = Animator.StringToHash("Vertical");
+        _isIdleHash = Animator.StringToHash("IsIdle");
+        _isJumpingHash = Animator.StringToHash("IsJumping");
+        _isFallingHash = Animator.StringToHash("IsFalling");
     }
 
     // Update is called once per frame
@@ -16,7 +26,28 @@ public class PlayerAnimator : MonoBehaviour
     {
         if (_animator == null) return;
         
-        _animator.SetFloat("Horizontal", horizontal * offset);
-        _animator.SetFloat("Vertical", vertical * offset);
+        float scaledHorizontal = horizontal * offset;
+        float scaledVertical = vertical * offset;
+        
+        _animator.SetFloat(_horizontalHash, scaledHorizontal);
+        _animator.SetFloat(_verticalHash, scaledVertical);
+
+        bool isIdle = Mathf.Abs(scaledHorizontal) < 0.1f && 
+                      Mathf.Abs(scaledVertical) < 0.1f;
+        _animator.SetBool(_isIdleHash, isIdle);
+    }
+    
+    public void SetJumpAnim(bool isJumping, bool isFalling)
+    {
+        if (_animator == null) return;
+
+        _animator.SetBool(_isJumpingHash, isJumping);
+        //_animator.SetBool(_isFallingHash, isFalling);
+        
+        // 점프/낙하 중에는 Idle 상태 해제
+        if (isJumping || isFalling)
+        {
+            _animator.SetBool(_isIdleHash, false);
+        }
     }
 }
