a8901d0ba4b5986a4d9116a18a4e2c704ce727e1
zuhwan0205 <ljh20230379@gmail.com>
2025-03-03 16:54:06 +0900

    SOL-157<InventoryUI - 들고 있는 아이템 하이라이트>

---
+++ Assets/Scripts/GameScene/Player/SlotHighlight.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class SlotHighlight : MonoBehaviour
+{
+    public Transform[] slotTransforms;
+    public Vector3 defaultScale = Vector3.one;
+    public Vector3 highlightScale = Vector3.one * 1.2f;
+
+    private void Awake()
+    {
+        slotTransforms = new Transform[5];
+        
+        GameObject slotContainer = GameObject.Find("SlotContainer");
+        if (slotContainer != null)
+        {
+            slotTransforms[0] = slotContainer.transform.Find("Slot01");
+            slotTransforms[1] = slotContainer.transform.Find("Slot02");
+            slotTransforms[2] = slotContainer.transform.Find("Slot03");
+            slotTransforms[3] = slotContainer.transform.Find("Slot04");
+            slotTransforms[4] = slotContainer.transform.Find("Slot05");
+        }
+        else
+        {
+            Debug.LogError("SlotContainer가 업씀");
+        }
+    }
+
+    public void UpdateSlotHighlight(int selectedSlot)
+    {
+        for (int i = 0; i < slotTransforms.Length; i++)
+        {
+            if (slotTransforms[i] != null)
+            {
+                slotTransforms[i].localScale = (i == selectedSlot)
+                    ? highlightScale
+                    : defaultScale;
+            }
+        }
+    }
+}
