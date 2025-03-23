dfa84420079a9e4c827da8dd3f4b3820b9f26a71
42 <daechoi@student.42seoul.kr>
2025-03-24 04:56:45 +0900

    SOL-216 <Player Stamina>

--- Assets/Scripts/GameScene/Manager/EventManager_Game.cs
+++ Assets/Scripts/GameScene/Manager/EventManager_Game.cs
@@ -9,6 +9,7 @@ public class EventManager_Game : MonoBehaviour
     public event Action<float, float> OnPlayerMove;
     public event Action<bool> OnPlayerSprint;
     public event Action OnPlayerJump;
+    public event Action<bool> OnPlayerSprintWithStamina;
 
 
     // Player Active
@@ -219,5 +220,10 @@ public class EventManager_Game : MonoBehaviour
     public void InvokeOptionConfirmButtonClicked()
     {
         OnOptionConfirmButtonClicked.Invoke();
+    }
+
+    public void InvokeOnPlayerSprintWithStamina(bool canSprint)
+    {
+        OnPlayerSprintWithStamina.Invoke(canSprint);
     }
 }
