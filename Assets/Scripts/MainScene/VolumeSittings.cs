6dd21518b6d523a9a895b174bc39a1774b5f5c08
zuhwan0205 <ljh20230379@gmail.com>
2025-04-05 21:41:27 +0900

    SOL-312<설정 저장>

--- Assets/Scripts/MainScene/VolumeSittings.cs
+++ Assets/Scripts/MainScene/VolumeSittings.cs
@@ -9,6 +9,8 @@ public class VolumeSittings : MonoBehaviour
     [SerializeField] private Slider MusicSlider;
     [SerializeField] private Slider micVolumeSlider;
     [SerializeField] private AudioMixer voiceMixer;
+    private const string MusicVolumeKey = "MusicVolume";
+    private const string MicVolumeKey = "MicVolume";
 
     private void Start()
     {
@@ -24,8 +26,13 @@ public class VolumeSittings : MonoBehaviour
         {
             SetMusicVolume();
         }
+        
+        float savedMicVolume = PlayerPrefs.GetFloat(MicVolumeKey, 1f);
+        micVolumeSlider.value = savedMicVolume;
+        SetMicVolume(savedMicVolume);
+        
+        MusicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
         micVolumeSlider.onValueChanged.AddListener(SetMicVolume);
-        micVolumeSlider.value = 0f;
     }
 
     public void SetMusicVolume()
@@ -41,16 +48,21 @@ public class VolumeSittings : MonoBehaviour
             Debug.LogWarning("VolumeSetting: AudioManager 또는 bgmSource가 없습니다.");
         }
 
-        PlayerPrefs.SetFloat("MainBGM", volume);
+        PlayerPrefs.SetFloat(MusicVolumeKey, volume);
+        PlayerPrefs.Save();
     }
 
     private void LoadVolume()
     {
-        MusicSlider.value = PlayerPrefs.GetFloat("MainBGM");
+        MusicSlider.value = PlayerPrefs.GetFloat(MusicVolumeKey);
         SetMusicVolume();
     }
     private void SetMicVolume(float value)
     {
-        voiceMixer.SetFloat("Master", value);
+        float dB =  Mathf.Clamp(value, -80f, 20f);
+        voiceMixer.SetFloat("Master", dB);
+
+        PlayerPrefs.SetFloat(MicVolumeKey, value);
+        PlayerPrefs.Save();
     }
 }
