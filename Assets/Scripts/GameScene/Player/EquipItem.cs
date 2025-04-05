d09c0cb3f42d036f83ab6ece6009cae14da433dc
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-04-05 20:35:43 +0900

    SOL-311 <Debug_TpsAnimLayer>

--- Assets/Scripts/GameScene/Player/EquipItem.cs
+++ Assets/Scripts/GameScene/Player/EquipItem.cs
@@ -187,7 +187,7 @@ public class EquipItem : MonoBehaviourPunCallbacks
         {
             UnequipArmRotation(() =>
             {
-                EventManager_Game.Instance.InvokeAnimationStateChange("Default");
+                // EventManager_Game.Instance.InvokeAnimationStateChange("Default");
             });
         }
     }
