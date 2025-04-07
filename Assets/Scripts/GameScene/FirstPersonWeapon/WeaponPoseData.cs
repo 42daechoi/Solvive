dbbf37c796a7f9e765f5b9e4abf298f5d9a5d9c8
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-04-07 12:10:39 +0900

    SOL-323 <FPS rot조절>

--- Assets/Scripts/GameScene/FirstPersonWeapon/WeaponPoseData.cs
+++ Assets/Scripts/GameScene/FirstPersonWeapon/WeaponPoseData.cs
@@ -7,6 +7,12 @@ public class WeaponPoseData
     public Vector3 r_ArmStrech;
     public Vector3 r_ForearmRotation;
     public Vector3 r_HandRotation;
+    public Vector3 index_r;
+    public Vector3 index_r2;
+    public Vector3 middle_r;
+    public Vector3 pinky_r;
+    public Vector3 ring_r;
+    public Vector3 thumb_r;
     public Vector3 l_ArmStrech;
     public Vector3 l_ForearmRotation;
     public Vector3 l_HandRotation;
