30da19862416b040ccce3ca2c5bbed8b09919c89
42 <daechoi@student.42seoul.kr>
2025-04-08 10:28:58 +0900

    SOL-331 <Generator - 배터리 회수 동시성 디버깅>

--- Assets/Scripts/GameScene/Interact/Generator.cs
+++ Assets/Scripts/GameScene/Interact/Generator.cs
@@ -6,33 +6,31 @@ using System.Collections;
 public class Generator : MonoBehaviourPun, IInteractableObject
 {
 	[SerializeField] private bool isComputerPhase;
-	[SerializeField] private int installedBatteryCount;
+	[SerializeField] private int installedBatteryCount = 0;
 	[SerializeField] private GameObject[] installedBattery;
 	private GeneratorSound generatorSound;
 	private GeneratorVibration generatorVibration;
-    private int maxBatteryCount = 3;
-    private Vector3 batteryPositionOffset;
+	private int maxBatteryCount = 3;
+	private Vector3 batteryPositionOffset;
 
-    private IEnumerator WaitForEventManager()
-    {
-        while (EventManager_Game.Instance == null)
-        {
-            Debug.Log("Generator : EventManager_Game 초기화 대기 중");
-            yield return new WaitForSeconds(0.1f);
-        }
-        EventManager_Game.Instance.OnAllGeneratorsActivated += OnChangePhase;
-        Debug.Log("Generator : EventManager_Game 초기화 완료");
-    }
+	private IEnumerator WaitForEventManager()
+	{
+		while (EventManager_Game.Instance == null)
+		{
+			yield return new WaitForSeconds(0.1f);
+		}
+		EventManager_Game.Instance.OnAllGeneratorsActivated += OnChangePhase;
+	}
 
-    private void OnEnable()
-    {
-        StartCoroutine(WaitForEventManager());
-    }
+	private void OnEnable()
+	{
+		StartCoroutine(WaitForEventManager());
+	}
 
-    private void OnDisable()
-    {
-        EventManager_Game.Instance.OnAllGeneratorsActivated -= OnChangePhase;
-    }
+	private void OnDisable()
+	{
+		EventManager_Game.Instance.OnAllGeneratorsActivated -= OnChangePhase;
+	}
 
 	void Start()
 	{
@@ -44,128 +42,120 @@ public class Generator : MonoBehaviourPun, IInteractableObject
 		{
 			generatorSound = _generatorSound;
 		}
-		else
-		{
-			Debug.LogError("Generator : GeneratorSound를 찾을 수 없습니다.");
-		}
 		if (TryGetComponent(out GeneratorVibration _generatorVibration))
 		{
 			generatorVibration = _generatorVibration;
 		}
-		else
-		{
-			Debug.LogError("Generator : GeneratorVibration을 찾을 수 없습니다.");
-		}
 	}
 
 
-    public void Interact(int playerID)
+	public void Interact(int playerID)
 	{
-		if (installedBatteryCount != 0 && !isComputerPhase)
+		if (installedBatteryCount > 0 && !isComputerPhase)
 		{
-			if (IsAllBatteryInstalled())
-			{
-                photonView.RPC("GeneratorStateChange", RpcTarget.All);
-            }
-            UninstallBattery(playerID);
-        }
+			photonView.RPC(nameof(UninstallBattery), RpcTarget.MasterClient, playerID);
+		}
 	}
 
 	public void TryInstallBattery(int playerID)
 	{
-        if (!PhotonNetwork.IsMasterClient)
-        {
+		if (!PhotonNetwork.IsMasterClient)
+		{
 			PhotonView playerPV = PhotonView.Find(playerID);
 			if (playerPV == null) return;
-            photonView.RPC("RequestInstallBattery", RpcTarget.MasterClient, playerPV.ViewID);
+			photonView.RPC(nameof(RequestInstallBattery), RpcTarget.MasterClient, playerPV.ViewID);
 			return;
-        }
-        if (IsAllBatteryInstalled())
+		}
+		if (IsAllBatteryInstalled())
 		{
 			Debug.Log("Generator : 배터리가 이미 가득 찼습니다.");
 			return;
 		}
 		InstallBattery(playerID);
-		if (IsAllBatteryInstalled())
-		{
-			photonView.RPC("GeneratorStateChange", RpcTarget.All);
-		}
-	}
-
-    [PunRPC]
-    private void RequestInstallBattery(int playerID)
-    {
-        if (!PhotonNetwork.IsMasterClient) return;
-		try
-		{
-            TryInstallBattery(playerID); 
-		}
-		catch (NullReferenceException e)
-		{
-			Debug.LogError($"Generator : {e.Message}");
-		}
-	}
-
-    private void InstallBattery(int playerID)
-	{
-        PhotonView playerPV = PhotonView.Find(playerID);
-        HeldItem heldItem = playerPV.GetComponent<HeldItem>();
-
-        Vector3 worldPosition = transform.position + transform.right * batteryPositionOffset.x
-                 + transform.up * batteryPositionOffset.y
-                 + transform.forward * batteryPositionOffset.z;
-
-        GameObject playerObject = heldItem.gameObject;
-		int viewID = playerObject.GetPhotonView().ViewID;
-		photonView.RPC("BatteryObjectSync", RpcTarget.All, true, viewID, worldPosition);
-		photonView.RPC("IncreaseBatteryCount", RpcTarget.All);
-	}
-
-	private void UninstallBattery(int playerId)
-	{
-		photonView.RPC("DecreaseBatteryCount", RpcTarget.All);
-		GameObject uninstallBatteryObject = installedBattery[installedBatteryCount];
-		FarmingObject farmingObject = uninstallBatteryObject.GetComponent<FarmingObject>();
-		farmingObject.Interact(playerId);
-		photonView.RPC("BatteryObjectSync", RpcTarget.All, false, 0, Vector3.zero);
-		installedBattery[installedBatteryCount] = null;
+		
 	}
 
 	[PunRPC]
-	private void BatteryObjectSync(bool isInstall, int playerViewID, Vector3 worldPosition)
+	private void RequestInstallBattery(int playerID)
 	{
-        try
-        {
-            if (isInstall)
-            {
-                PhotonView playerPV = PhotonView.Find(playerViewID);
-				HeldItem heldItem = playerPV.GetComponent<HeldItem>();
-                installedBattery[installedBatteryCount] = heldItem.GetItemObject();
+		if (!PhotonNetwork.IsMasterClient) return;
+		TryInstallBattery(playerID); 
+	}
 
-                heldItem.ReplaceItem(worldPosition, false);
-                photonView.RPC("SyncParent", RpcTarget.All, installedBattery[installedBatteryCount].GetComponent<PhotonView>().ViewID);
-            }
-            else
-            {
-                installedBattery[installedBatteryCount] = null;
-            }
-        }
-        catch (NullReferenceException e)
-        {
-            Debug.LogError($"Generator : {e.Message}");
-        }
-    }
+	private void InstallBattery(int playerID)
+	{
+		PhotonView playerPV = PhotonView.Find(playerID);
+		HeldItem heldItem = playerPV.GetComponent<HeldItem>();
 
-    [PunRPC]
-    private void SyncParent(int itemPhotonViewID)
-    {
-        PhotonView itemPhotonView = PhotonView.Find(itemPhotonViewID);
-        if (itemPhotonView != null)
-        {
-            itemPhotonView.transform.SetParent(this.gameObject.transform);
-        }
-    }
-    [PunRPC]
+		Vector3 worldPosition = transform.position + transform.right * batteryPositionOffset.x
+				 + transform.up * batteryPositionOffset.y
+				 + transform.forward * batteryPositionOffset.z;
+
+		GameObject playerObject = heldItem.gameObject;
+		int viewID = playerObject.GetPhotonView().ViewID;
+		int prevCount = installedBatteryCount;
+		photonView.RPC(nameof(BatterySync), RpcTarget.All, true, viewID, worldPosition);
+		StartCoroutine(WaitAndCheckGeneratorState(prevCount, installedBatteryCount));
+	}
+
+	[PunRPC]
+	private void UninstallBattery(int playerId)
+	{
+		if (installedBatteryCount <= 0 || installedBattery[installedBatteryCount - 1] == null)
+		{
+			return;
+		}
+
+		GameObject uninstallBatteryObject = installedBattery[installedBatteryCount - 1];
+		FarmingObject farmingObject = uninstallBatteryObject.GetComponent<FarmingObject>();
+
+		if (!farmingObject.GetIsPickUp())
+		{
+			farmingObject.Interact(playerId);
+			int prevCount = installedBatteryCount;
+			photonView.RPC(nameof(BatterySync), RpcTarget.All, false, 0, Vector3.zero);
+
+			StartCoroutine(WaitAndCheckGeneratorState(prevCount, installedBatteryCount));
+		}
+	}
+
+	private IEnumerator WaitAndCheckGeneratorState(int prevCount, int currCount)
+	{
+		yield return new WaitForSeconds(0.05f);
+
+		photonView.RPC(nameof(GeneratorStateChange), RpcTarget.All, prevCount, currCount);
+	}
+
+	[PunRPC]
+	private void BatterySync(bool isInstall, int playerViewID, Vector3 worldPosition)
+	{
+		if (isInstall)
+		{
+			PhotonView playerPV = PhotonView.Find(playerViewID);
+			HeldItem heldItem = playerPV.GetComponent<HeldItem>();
+			installedBattery[installedBatteryCount] = heldItem.GetItemObject();
+	
+			heldItem.ReplaceItem(worldPosition, false);
+			photonView.RPC(nameof(SyncParent), RpcTarget.All, installedBattery[installedBatteryCount].GetComponent<PhotonView>().ViewID);
+			IncreaseBatteryCount();
+		}
+		else
+		{   
+			installedBattery[installedBatteryCount - 1] = null;
+			DecreaseBatteryCount();
+		}
+	}
+
+	[PunRPC]
+	private void SyncParent(int itemPhotonViewID)
+	{
+		PhotonView itemPhotonView = PhotonView.Find(itemPhotonViewID);
+		if (itemPhotonView != null)
+		{
+			itemPhotonView.transform.SetParent(this.gameObject.transform);
+		}
+	}
+
 	private void IncreaseBatteryCount()
 	{
 		installedBatteryCount++;
@@ -174,30 +164,30 @@ public class Generator : MonoBehaviourPun, IInteractableObject
 		Debug.Log($"Generator : 배터리 장착 성공. 현재 장착된 배터리 갯수 : {installedBatteryCount}");
 	}
 
-	[PunRPC]
 	private void DecreaseBatteryCount()
 	{
 		installedBatteryCount--;
 		batteryPositionOffset -= new Vector3(0.4f, 0, 0);
-        generatorSound.PlayInstallBatterySound();
-        Debug.Log($"Generator : 배터리 회수 성공. 현재 장착된 배터리 갯수 : {installedBatteryCount}");
+		generatorSound.PlayInstallBatterySound();
+		Debug.Log($"Generator : 배터리 회수 성공. 현재 장착된 배터리 갯수 : {installedBatteryCount}");
 	}
 
 	[PunRPC]
-	private void GeneratorStateChange()
+	private void GeneratorStateChange(int prevCount, int currCount)
 	{
-		if (IsAllBatteryInstalled())
+		Debug.Log($"Generator : battery count {prevCount}, {currCount}");
+		if (prevCount == 2 && currCount == 3)
 		{
 			generatorVibration.SetIsGeneratorActive(true);
 			generatorSound.PlayStartSound();
-            GameManager.Instance.AddActiveGenerator();
+			GameManager.Instance.AddActiveGenerator();
 		}
-		else
+		else if (prevCount == 3 && currCount == 2)
 		{
-            generatorVibration.SetIsGeneratorActive(false);
+			generatorVibration.SetIsGeneratorActive(false);
 			generatorSound.PlayEndSound();
-            GameManager.Instance.SubActiveGenerator();
-        }
+			GameManager.Instance.SubActiveGenerator();
+		}
 	}
 
 	private bool IsAllBatteryInstalled()
